9dadfc9cab5433f5969a919789888ea70c3d3aba
karthiga25shree-crypto <karthiga25shree@gmail.com>
2026-09-17 18:55:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("🍦 Welcome to Sweet Scoop! Check out our delicious flavours!");
+}
+
+function claimOffer() {
+    alert("🎉 Congratulations! You received 20% OFF on your first order!");
+}
+
+function contactShop() {
+    alert("📍 Sweet Scoop Ice Cream\nChennai, Tamil Nadu\n\nWe are happy to serve you! 🍨");
+}
